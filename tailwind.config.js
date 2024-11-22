e4efd9081b76ec52633958648d2f1488dee1c181
ASNProject <asnproject02@gmail.com>
2024-11-22 11:35:36 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        lightBlue: "#0070C0",
+      }
+    },
+  },
+  plugins: [],
+}
+
